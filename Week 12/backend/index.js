195bae1d7c03c37f17554d9f0d7fa38bd07fa037
MinhTrinh04 <minhfananime@gmail.com>
2025-12-05 09:38:19 +0700

    backend

---
+++ Week 12/backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Student = require('./Student');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/student_db')
+    .then(() => console.log("Đã kết nối MongoDB thành công"))
+    .catch(err => console.error("Lỗi kết nối MongoDB:", err));
+
+app.get('/api/students', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
